ecef0805cf4e463af02508a8653dac12278e422a
Tae Kyoun Kim <xorbskim6105@gmail.com>
2025-12-28 14:36:33 +0900

    feat : 명령 입력 기능 추가

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,41 @@
 package pairmatching;
 
+import java.util.List;
+import pairmatching.view.InputView;
+import pairmatching.view.MdFileReader;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 구현 진행
+        MdFileReader mdFileReader = new MdFileReader();
+
+//        1. 파일 입력 및 사전 작업
+//            1. 백엔드 읽어오기
+        String beFileName = "src/main/resources/backend-crew.md";
+        List<String> beMember = mdFileReader.readMdFile(beFileName);
+
+//            2. 프론트 읽어오기
+        String feFileName = "src/main/resources/backend-crew.md";
+        List<String> feMember = mdFileReader.readMdFile(feFileName);
+
+//        2. 기능 입력 받기
+        String command = InputView.readCommand();
+//        3. 페어 매칭 기능
+//            1. 과정, 레벨, 미션 입력 받기
+//            2. 멤버 목록에서 페어 만들기
+//                1. 새롭게 만드는 경우/같은과정,다른레벨,다른 미션,등등
+//                    1. 그냥 만들 면됨
+//                2. 같은 과정, 같은 레벨, 다른 미션에서 만드는 경우(if)
+//                    1. 멤버끼리 비교해서 페어가 되었던 적이 있으면 다시 시도(같은레벨 다른 미션)
+//            3. 해당 미션에 대한 페어 정보 저장해두기
+//        4. 페어 조회 기능
+//            1. 과정, 레벨, 미션 입력 받기
+//            2. 과정, 레벨, 미션에 대한 페어 정보 찾기
+//            3. 결과 출력하기
+//                1. 페어 정보 출력
+//                2. 없을 경우 에러 메시지 출력
+//        5. 페어 초기화 기능
+//            1. 기존에 존재하던 페어 전부 초기화 하기
+
     }
 }
--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -3,14 +3,34 @@ package pairmatching.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public String readInput(){
+
+    public static String readCommand(){
+        System.out.println("기능을 선택하세요.");
+        System.out.println("1. 페어 매칭\n"
+                + "2. 페어 조회\n"
+                + "3. 페어 초기화\n"
+                + "Q. 종료");
+        try{
+            return Console.readLine();
+        }catch (IllegalArgumentException e){
+            throw new IllegalArgumentException(e.getMessage());
+        }
+    }
+
+
+
+
+
+    public static String readInput(){
         System.out.println("입력받는 메서드");
         String input = Console.readLine();
         return input;
     }
 
+
+
     // 오류 발생 지점 부터 재 입력 받는 로직
-    public String readInputWithRetry(){
+    public static String readInputWithRetry(){
         System.out.println("이 부분이 반복 됩니다.");
         try{
             String input = Console.readLine();
