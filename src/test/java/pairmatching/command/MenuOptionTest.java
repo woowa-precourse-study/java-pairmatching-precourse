0f835fd88cac742d12134b25da22d27df50d929c
khcho96 <khcho1492@gmail.com>
2026-01-03 13:55:31 +0900

    test: 테스트 추가

--- src/test/java/pairmatching/command/MenuOptionTest.java
+++ src/test/java/pairmatching/command/MenuOptionTest.java
@@ -1,5 +1,6 @@
 package pairmatching.command;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
@@ -11,5 +12,11 @@ class MenuOptionTest {
         assertThatThrownBy(() -> MenuOption.from("4"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorMessage.INVALID_FORMAT.getErrorMessage());
+    }
+
+    @Test
+    void 기능_입력_정상_작동() {
+        MenuOption option = MenuOption.from("3");
+        assertThat(option).isEqualTo(MenuOption.C);
     }
 }
